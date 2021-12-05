74698953ad63dadf9c9b9575f244e60cb3ae1c2a
lewin <llw13025378994@gmail.com>
2021-12-06 00:25:23 +0800

    fix

--- .vuepress/sidebar.js
+++ .vuepress/sidebar.js
@@ -6,5 +6,5 @@ const JS_PATH = '/blogs/javascript'
 module.exports = {
   [CSS_PATH]: [createSideBarConfig('CSS 技巧', CSS_PATH)],
   [NODE_PATH]: [createSideBarConfig('Node.js 技术', NODE_PATH)],
-  [JS_PATH]: [createSideBarConfig('手写JS常见面试题合集', JS_PATH + 'js-methods')]
+  [JS_PATH]: [createSideBarConfig('手写JS常见面试题合集', JS_PATH + '/js-methods')]
 }
