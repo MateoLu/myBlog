753a9f4b6c0f8975966941e8dbe8ab6e68b63b27
lewin <llw13025378994@gmail.com>
2021-12-06 23:33:44 +0800

    new

--- .vuepress/sidebar.js
+++ .vuepress/sidebar.js
@@ -2,9 +2,11 @@ const { createSideBarConfig } = require('./util')
 const CSS_PATH = '/blogs/css'
 const NODE_PATH = '/blogs/node'
 const JS_PATH = '/blogs/javascript'
+const PREVIEW_PATH = '/blogs/preview'
 
 module.exports = {
   [CSS_PATH]: [createSideBarConfig('CSS 技巧', CSS_PATH)],
   [NODE_PATH]: [createSideBarConfig('Node.js 技术', NODE_PATH)],
-  [JS_PATH]: [createSideBarConfig('手写JS常见面试题合集', JS_PATH + '/js-methods')]
+  [JS_PATH]: [createSideBarConfig('手写JS常见面试题合集', JS_PATH + '/js-methods')],
+  [PREVIEW_PATH]: [createSideBarConfig('常考面试题集合', PREVIEW_PATH)]
 }
--- blogs/javascript/js-methods/array.md
+++ blogs/javascript/js-methods/array.md
@@ -4,7 +4,7 @@ date: 2021-12-5
 categories:
  - JavaScript
 tags:
- - JS
+ - JS 面试相关
 sticky: 1
 ---
 
